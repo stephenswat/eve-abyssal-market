14290b251ae6fcaacb11f44da849fa4e04c1a981
Stephen Swatman <stephenswat@gmail.com>
2018-05-22 23:29:42 +0200

    Allow the ESI app to use Sisi

--- eve_esi/__init__.py
+++ eve_esi/__init__.py
@@ -3,14 +3,17 @@ import esipy
 from django.conf import settings
 
 
-ESI_APP = esipy.App.create(settings.ESI_SWAGGER_JSON)
+ESI_APP = esipy.EsiApp(
+    datasource=getattr(settings, 'ESI_DATASOURCE', 'tranquility')
+)
 
 
 ESI_SECURITY = esipy.EsiSecurity(
-    app=ESI_APP,
+    app=ESI_APP.get_latest_swagger,
     redirect_uri=settings.ESI_CALLBACK,
     client_id=settings.ESI_CLIENT_ID,
     secret_key=settings.ESI_SECRET_KEY,
+    esi_datasource=getattr(settings, 'ESI_DATASOURCE', 'tranquility')
 )
 
 
