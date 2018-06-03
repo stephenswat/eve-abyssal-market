af15a8d5c26f5b320d852e6482c456d4bb288ab4
Stephen Swatman <stephenswat@gmail.com>
2018-06-03 21:09:37 +0200

    I was using EsiPy wrongly, fix that

--- eve_auth/views.py
+++ eve_auth/views.py
@@ -2,18 +2,22 @@ from django.contrib.auth import authenticate, login
 from django.views.generic.base import View
 from django.shortcuts import redirect
 
-from eve_esi import ESI_SECURITY
+from eve_esi import get_esi_security
 
 
 class LoginView(View):
     def get(self, *args, **kwargs):
-        return redirect(ESI_SECURITY.get_auth_uri())
+        return redirect(get_esi_security().get_auth_uri(
+            scopes=['esi-contracts.read_character_contracts.v1', 'esi-assets.read_assets.v1']
+        ))
 
 
 class CallbackView(View):
     def get(self, request, *args, **kwargs):
-        ESI_SECURITY.auth(request.GET['code'])
-        data = ESI_SECURITY.verify()
+        security = get_esi_security()
+
+        security.auth(request.GET['code'])
+        data = security.verify()
 
         user = authenticate(
             request,
--- eve_esi/__init__.py
+++ eve_esi/__init__.py
@@ -8,17 +8,19 @@ ESI_APP = esipy.EsiApp(
 )
 
 
-ESI_SECURITY = esipy.EsiSecurity(
-    app=ESI_APP.get_latest_swagger,
-    redirect_uri=settings.ESI_CALLBACK,
-    client_id=settings.ESI_CLIENT_ID,
-    secret_key=settings.ESI_SECRET_KEY,
-    esi_datasource=getattr(settings, 'ESI_DATASOURCE', 'tranquility'),
-    headers={'User-Agent': settings.ESI_USER_AGENT}
-)
+def get_esi_security():
+    return esipy.EsiSecurity(
+        app=ESI_APP.get_latest_swagger,
+        redirect_uri=settings.ESI_CALLBACK,
+        client_id=settings.ESI_CLIENT_ID,
+        secret_key=settings.ESI_SECRET_KEY,
+        esi_datasource=getattr(settings, 'ESI_DATASOURCE', 'tranquility'),
+        headers={'User-Agent': settings.ESI_USER_AGENT}
+    )
 
 
-ESI_CLIENT = esipy.EsiClient(
-    security=ESI_SECURITY,
-    headers={'User-Agent': settings.ESI_USER_AGENT}
-)
+def get_esi_client(security=None):
+    return esipy.EsiClient(
+        security=security,
+        headers={'User-Agent': settings.ESI_USER_AGENT}
+    )
