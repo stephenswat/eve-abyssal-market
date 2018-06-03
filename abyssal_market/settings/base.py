8c4dc85a7d522ec6ba9e25e7ffeeaa01390d90e9
Stephen Swatman <stephenswat@gmail.com>
2018-06-03 20:55:56 +0200

    Add a simple home page

--- abyssal_market/settings/base.py
+++ abyssal_market/settings/base.py
@@ -32,7 +32,7 @@ ROOT_URLCONF = 'abyssal_market.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- abyssal_market/urls.py
+++ abyssal_market/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('eve_auth.urls', namespace='eve_auth')),
+    path('', TemplateView.as_view(template_name='home.html'))
 ]
