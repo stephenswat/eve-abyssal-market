6ff20cd503a85695297c502f3a38538e8e24e56d
Stephen Swatman <stephenswat@gmail.com>
2018-06-04 00:11:30 +0200

    Add a contract data model

--- contract_scanner/admin.py
+++ contract_scanner/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from contract_scanner.models import Contract
+
+
+admin.site.register(Contract)
--- contract_scanner/models.py
+++ contract_scanner/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from eve_auth.models import EveUser
+
+
+class Contract(models.Model):
+    STATUS_CHOICES = (
+        (0, 'Available'),
+        (1, 'Expired'),
+        (2, 'Sold'),
+        (3, 'Disappeared'),
+    )
+
+    id = models.IntegerField(primary_key=True)
+
+    status = models.SmallIntegerField(db_index=True, choices=STATUS_CHOICES)
+
+    issuer_id = models.BigIntegerField(db_index=True)
+    owner = models.ForeignKey(EveUser, models.SET_NULL, db_index=True, null=True)
+
+    price = models.DecimalField(max_digits=32, decimal_places=2, db_index=True)
+
+    issued_at = models.DateTimeField()
+    expires_at = models.DateTimeField()
+
+    seen_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
