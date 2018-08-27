a6b493b1b72bc5f2a63f3827faacf0646abb2d15
Stephen Swatman <stephenswat@gmail.com>
2018-08-28 01:57:19 +0200

    Add a copy link button

--- static/js/mutaplasmid.js
+++ static/js/mutaplasmid.js
@@ -5,3 +5,28 @@ function open_contract(cid, csrf) {
         data: { contract_id: cid, csrfmiddlewaretoken: csrf }
     });
 }
+
+clipboard = new ClipboardJS('.btn-copy-contract-link');
+
+$('button').tooltip({
+    trigger: 'click',
+    placement: 'top'
+});
+
+
+function setTooltip(btn, message) {
+    $(btn).tooltip('hide')
+        .attr('data-original-title', message)
+        .tooltip('show');
+}
+
+function hideTooltip(btn) {
+    setTimeout(function () {
+        $(btn).tooltip('hide');
+    }, 1000);
+}
+
+clipboard.on('success', function (e) {
+    setTooltip(e.trigger, 'Copied!');
+    hideTooltip(e.trigger);
+});
