6fc40a6ddc1a55ab22ac1f8451e7f5e7c2c318a9
Stephen Swatman <stephenswat@gmail.com>
2018-08-29 15:55:17 +0200

    Add EVE characters to the admin site

--- abyssal_modules/admin.py
+++ abyssal_modules/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from abyssal_modules.models import ModuleType, ModuleDogmaAttribute, Module
+from abyssal_modules.models import ModuleType, ModuleDogmaAttribute, Module, EveCharacter
 
 
 admin.site.register(ModuleType)
 admin.site.register(ModuleDogmaAttribute)
 admin.site.register(Module)
+admin.site.register(EveCharacter)
