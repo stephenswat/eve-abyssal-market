3d81a166d50ed2d3ea2829fd5ec6d21f11543a56
Stephen Swatman <stephenswat@gmail.com>
2018-09-05 00:21:09 +0200

    Add a robots.txt

--- abyssal_market/urls.py
+++ abyssal_market/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('auth/', include('eve_auth.urls', namespace='eve_auth')),
     path('faq/', TemplateView.as_view(template_name="help.html"), name='faq'),
     path('legal/', TemplateView.as_view(template_name="legal.html"), name='legal'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt")),
     path('', include('abyssal_modules.urls', namespace='abyssal_modules'))
 ]
 
