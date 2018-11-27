52c57ca7c9a8e3a966cc12f542d00f2d141d3c24
Stephen Swatman <stephenswat@gmail.com>
2018-11-27 13:08:27 +0100

    Lower level cache should work better

--- abyssal_api/views.py
+++ abyssal_api/views.py
@@ -1,12 +1,10 @@
+from django.core.cache import cache
 from django.http import JsonResponse, Http404
 from django.views import View
-from django.views.decorators.cache import cache_page
-from django.utils.decorators import method_decorator
 
 from abyssal_modules.models import Module, ModuleType
 
 
-@method_decorator(cache_page(60 * 10), name='dispatch')
 class AvailableTypedModuleListAPI(View):
     def get(self, request, type_id):
         try:
@@ -14,21 +12,30 @@ class AvailableTypedModuleListAPI(View):
         except ModuleType.DoesNotExist:
             raise Http404("Module type does not exist.")
 
-        return JsonResponse([
-            {
-                'id': m.id,
-                'attributes': m.attribute_dict_with_derived(),
-                'contract': {
-                    'id': m.contract_id,
-                    'price': {
-                        'isk': m.contract_price,
-                        'plex': m.contract_plex,
-                        'total': m.contract_price_inc_plex
+        key = "AvailableTypedModuleListAPI_%d" % type_id
+
+        res = cache.get(key)
+
+        if res is None:
+            res = [
+                {
+                    'id': m.id,
+                    'attributes': m.attribute_dict_with_derived(),
+                    'contract': {
+                        'id': m.contract_id,
+                        'price': {
+                            'isk': m.contract_price,
+                            'plex': m.contract_plex,
+                            'total': m.contract_price_inc_plex
+                        },
+                        'auction': m.contract_auction,
+                        'multi_item': not m.contract_single
                     },
-                    'auction': m.contract_auction,
-                    'multi_item': not m.contract_single
-                },
-                'pyfa': m.get_pyfa_string()
-            }
-            for m in Module.available.filter(type=module_type)
-        ], safe=False)
+                    'pyfa': m.get_pyfa_string()
+                }
+                for m in Module.available.filter(type=module_type)
+            ]
+
+            cache.set(key, res, 60 * 15)
+
+        return JsonResponse(res, safe=False)
