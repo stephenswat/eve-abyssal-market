886b68386179cf1f858e3a96f1f1e8cf423739d8
Stephen Nicholas Swatman <stephenswat@gmail.com>
2023-01-15 15:55:42 +0100

    Add a donation page

--- abyssal_market/urls.py
+++ abyssal_market/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('api/', include('abyssal_api.urls', namespace='abyssal_api')),
     path('faq/', TemplateView.as_view(template_name="help.html"), name='faq'),
     path('legal/', TemplateView.as_view(template_name="legal.html"), name='legal'),
+    path('support/', TemplateView.as_view(template_name="support.html"), name='support'),
     path('robots.txt', TemplateView.as_view(template_name="robots.txt")),
     path('', include('abyssal_modules.urls', namespace='abyssal_modules')),
     path('', include('django_prometheus.urls')),
