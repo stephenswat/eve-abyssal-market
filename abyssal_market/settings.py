dad3e93938dfac770e00d5ebeae38a6d5766c132
Stephen Nicholas Swatman <stephenswat@gmail.com>
2023-01-23 23:23:56 +0100

    Fix the dropdown menu

--- abyssal_market/settings.py
+++ abyssal_market/settings.py
@@ -71,18 +71,6 @@ TEMPLATES = [
 
 # Default settings
 BOOTSTRAP5 = {
-    "css_url": {
-        "url": "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0-alpha1/css/bootstrap.min.css",
-        "integrity": "sha512-72OVeAaPeV8n3BdZj7hOkaPSEk/uwpDkaGyP4W2jSzAC8tfiO4LMEDWoL3uFp5mcZu+8Eehb4GhZWFwvrss69Q==",
-        "crossorigin": "anonymous",
-    },
-
-    "javascript_url": {
-        "url": "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0-alpha1/js/bootstrap.min.js",
-        "integrity": "sha512-eHx4nbBTkIr2i0m9SANm/cczPESd0DUEcfl84JpIuutE6oDxPhXvskMR08Wmvmfx5wUpVjlWdi82G5YLvqqJdA==",
-        "crossorigin": "anonymous",
-    },
-
     "theme_url": "/static/css/theme.css",
 }
 
