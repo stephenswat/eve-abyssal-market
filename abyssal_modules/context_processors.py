be7a9f36d7494666db8b0f7af13eb052986fe999
Stephen Nicholas Swatman <stephenswat@gmail.com>
2023-12-23 12:31:05 +0100

    Add ads to the appraisal page

--- abyssal_modules/context_processors.py
+++ abyssal_modules/context_processors.py
@@ -5,4 +5,7 @@ def google_analytics(request):
     return {"GA_TRACKING_ID": getattr(settings, "GA_TRACKING_ID", "")}
 
 def google_adsense(request):
-    return {"ADSENSE_OWNER_TAG": getattr(settings, "ADSENSE_OWNER_TAG", "")}
+    return {
+        "ADSENSE_PUBLISHER_ID": getattr(settings, "ADSENSE_PUBLISHER_ID", ""),
+        "ADSENSE_SLOTS": getattr(settings, "ADSENSE_SLOTS", None)
+    }
